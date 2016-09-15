72a284be0f9fd444c3a2a0ed21536c908610e46d
Jc Tan <omegachien@gmail.com>
2016-09-15 16:45:33 +0800

    Basic gulp house keeping

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Project DevFest</title>
+  <link rel="stylesheet" href="/css/app.css">
+</head>
+
+<body>
+  <div class="container">
+    @yield('content')
+  </div>
+
+</body>
+
+</html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+
+@section('content')
+  <h1>Project DevFest</h1>
+@stop
