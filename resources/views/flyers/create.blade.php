bb2120b2ff9f1db325dca9bf7af65f70eb5d3652
Jc Tan <omegachien@gmail.com>
2016-09-17 15:10:15 +0800

    - Added Create form - Added Country class to populate select box in form

--- resources/views/flyers/create.blade.php
+++ resources/views/flyers/create.blade.php
@@ -4,4 +4,10 @@
 
   <h1>Selling your home?</h1>
 
+  <hr>
+
+  <form method="POST" action="/flyers" enctype="multipart/form-data">
+    @include('flyers.form')
+  </form>
+
 @stop
---
+++ resources/views/flyers/form.blade.php
@@ -0,0 +1,48 @@
+<div class="form-group">
+  <label for="street">Street</label>
+  <input type="text" name="street" id="street" class="form-control" value="{{ old('street') }}">
+</div>
+
+<div class="form-group">
+  <label for="city">City</label>
+  <input type="text" name="city" id="city" class="form-control" value="{{ old('city') }}">
+</div>
+
+<div class="form-group">
+  <label for="zip">Zip / Postal Code</label>
+  <input type="text" name="zip" id="zip" class="form-control" value="{{ old('zip') }}">
+</div>
+
+<div class="form-group">
+  <label for="country">Country</label>
+  <select name="country" id="country" class="form-control">
+    @foreach(App\Http\Utilities\Country::all() as $name => $code)
+      <option value="{{ $code }}">{{ $name }}</option>
+    @endforeach
+
+  </select>
+</div>
+
+<div class="form-group">
+  <label for="state">State</label>
+  <input type="text" name="state" id="state" class="form-control" value="{{ old('state') }}">
+</div>
+
+<hr>
+
+<div class="form-group">
+  <label for="price">Sales Price</label>
+  <input type="text" name="price" id="price" class="form-control" value="{{ old('price') }}">
+</div>
+
+<div class="form-group">
+  <label for="description">Description</label>
+  <textarea rows="10" name="description" id="description" class="form-control">{{ old('description') }}</textarea>
+</div>
+
+<div class="form-group">
+  <label for="photos">Photos</label>
+  <input type="file" name="photos" id="photos" class="form-control" value="">
+</div>
+
+<button class="btn btn-primary">Create Flyer</button>
