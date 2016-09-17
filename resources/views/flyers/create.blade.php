e7b3e4e43be030f50eafdc45c26646114c487453
Jc Tan <omegachien@gmail.com>
2016-09-17 10:50:50 +0800

    step 4

---
+++ resources/views/flyers/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('content')
+
+  <h1>Selling your home?</h1>
+
+@stop
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,13 @@
 @extends('layout')
 
 @section('content')
-  <h1>Project DevFest</h1>
+  <div class="jumbotron">
+    <h1>Project DevFest</h1>
+    <p>
+      This is a template showcasing the optional theme.
+    </p>
+
+    <a href="/flyers/create" class="btn btn-primary">Create a Flyer</a>
+  </div>
 @stop
+
